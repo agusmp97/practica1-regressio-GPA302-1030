00b9a26b989d727869bcddf1bb226ff4d789ec88
agusmp97-uab <agustin.martinezp@e-campus.uab.cat>
2021-09-30 16:24:14 +0200

    Primer commit amb llibreries instalades

---
+++ main.py
@@ -0,0 +1,9 @@
+# Agustín Martínez Pérez (1528789)
+# Alexandre Moro Rialp (1527046)
+# David Sardà Martín (1492054)
+
+import numpy as np
+import sklearn as sk
+import matplotlib as mp
+import scipy as scp
+
