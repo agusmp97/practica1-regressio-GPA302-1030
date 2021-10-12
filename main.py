ecdd4afc1e3f038fbbb9c66ccd0c6f9bc5875723
agusmp97-uab <agustin.martinezp@e-campus.uab.cat>
2021-10-12 17:11:23 +0200

    main modificació no important

--- main.py
+++ main.py
@@ -6,5 +6,15 @@ import numpy as np
 import sklearn as sk
 import matplotlib as mp
 import scipy as scp
+import pandas as pd
 
 # Repo creat
+def load_dataset(path):
+    dataset = pd.read_csv(path, header=0, delimiter=",")
+    return dataset
+
+dataset_lebron = load_dataset("archive/lebron_career.csv")
+dataset_jordan = load_dataset("archive/jordan_career.csv")
+
+dtypes_lebron = dataset_lebron.dtypes
+print(dtypes_lebron)
